36f3d8ca330fa006cca86917254f9ce6963ebdd2
Ehigai Salvation <136333156+ehigai@users.noreply.github.com>
2026-06-07 17:06:45 +0100

    Removed unused variables

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,22 +1,7 @@
-import { useEffect, useState } from "react"
-import { api } from "@/lib/eden"
 import Board from "@/components/board"
 import { PieceSelector } from "./components/theme/piece-selector"
 
 export function App() {
-  const [data, setData] = useState<string | null>("")
-  const [rootData, setRootData] = useState<string | null>("")
-  useEffect(() => {
-    const fetchData = async () => {
-      const { data: rootData } = await api.get()
-      setRootData(rootData)
-
-      const { data: idData } = await api.id({ id: "string" }).get()
-      setData(idData)
-    }
-    fetchData()
-  }, [])
-
   return (
     <main className="flex h-screen w-full items-center justify-center">
       <Board />
