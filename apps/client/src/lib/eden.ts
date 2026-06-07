1efa88b2fbe5f9771231f41fc62606f634083330
Ehigai Salvation <136333156+ehigai@users.noreply.github.com>
2026-06-07 17:11:37 +0100

    Commented out unused code

--- apps/client/src/lib/eden.ts
+++ apps/client/src/lib/eden.ts
@@ -1,5 +1,5 @@
-import { treaty } from "@elysia/eden"
-import type { App } from "@server/index"
-import { env } from "./env"
+// import { treaty } from "@elysia/eden"
+// import type { App } from "@server/index"
+// import { env } from "./env"
 
-export const api = treaty<App>(env("VITE_API_URL"))
+// export const api = treaty<App>(env("VITE_API_URL"))
