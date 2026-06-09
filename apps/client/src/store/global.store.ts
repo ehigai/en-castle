f72963605efb9301ed58aa6795a141de871f3e23
Ehigai Salvation <136333156+ehigai@users.noreply.github.com>
2026-06-09 21:12:33 +0100

    feat(client): add notations visibility toggle

--- apps/client/src/store/global.store.ts
+++ apps/client/src/store/global.store.ts
@@ -3,17 +3,21 @@ import { create } from "zustand"
 interface GlobalState {
   isdeveloperMode: boolean
   autoFlip: boolean
+  showNotations: boolean
   toggleDeveloperMode: () => void
   toggleAutoFlip: () => void
+  toggleNotations: () => void
 }
 
 const useGlobalStore = create<GlobalState>((set) => ({
   isdeveloperMode: false,
   autoFlip: false,
+  showNotations: false,
   toggleDeveloperMode: () =>
     set((state) => ({ isdeveloperMode: !state.isdeveloperMode })),
-  toggleAutoFlip: () =>
-    set((state) => ({ autoFlip: !state.autoFlip })),
+  toggleAutoFlip: () => set((state) => ({ autoFlip: !state.autoFlip })),
+  toggleNotations: () =>
+    set((state) => ({ showNotations: !state.showNotations })),
 }))
 
 export default useGlobalStore
